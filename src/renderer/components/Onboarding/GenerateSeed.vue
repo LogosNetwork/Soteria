e52f190602f4d87a9cd8feb4c0dfb34b883079de
Tyler Storm <stormtv@mail.uc.edu>
2019-01-04 15:57:24 -0500

    Add generate seed page

--- src/renderer/components/Onboarding/GenerateSeed.vue
+++ src/renderer/components/Onboarding/GenerateSeed.vue
@@ -1,15 +1,27 @@
 <template>
-  <div class="h-100">
-    <b-container class="h-100">
-      <div class="row h-100 justify-content-center align-items-center">
-        Righteous
+  <div class="h-100" style="overflow:hidden">
+    <div class="row h-100 justify-content-center align-items-center">
+      <div class="panel table">
+        <div class="plan">
+          <div class="circle">
+            <font-awesome-icon size="6x" class="icon" :icon="['fal','file-import']" />
+          </div>
+          <h5 class="header" v-t="'haveaseed'"></h5>
+          <b-button class="mt-3" size="lg" v-t="'importseed'" variant="outline-primary"></b-button>
+        </div>
+        <div class="plan">
+          <div class="circle">
+            <font-awesome-icon size="6x" class="icon" :icon="['fal','chart-network']" />
+          </div>
+          <h5 class="header" v-t="'needaseed'"></h5>
+          <b-button class="mt-3" size="lg" v-t="'generateseed'" variant="primary"></b-button>
+        </div>
       </div>
-    </b-container>
+    </div>
     <b-row class="fixed-row-bottom">
       <b-col class="p-0 w-100">
         <b-button-group class="w-100" size="lg">
           <b-button class="w-100" variant="dark" v-t="'previous'"  v-on:click="changeLanguagePage()"></b-button>
-          <b-button class="w-100" variant="default" v-t="'continue'"  v-on:click="validateSeedPage()"></b-button>
         </b-button-group>
       </b-col>
     </b-row>
@@ -30,7 +42,7 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style scoped lang='scss'>
   #jumbotron {
     width:450px;
   }
@@ -39,5 +51,55 @@ export default {
     bottom: 0;
     left: 15px;
     width: 100vw;
+  }
+  .panel {
+    border-radius: 10px;
+    padding: 15px 25px;
+    position: relative;
+    width: 100%;
+    z-index: 10;
+  }
+  .icon {
+    color:#007bff;
+    max-width: 50%;
+  }
+
+  .circle {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 180px;
+    height: 180px;
+    background: #FFF;
+    border-radius: 50%;
+    margin: auto;
+    margin-bottom: 25px;
+    max-width: 100%;
+  }
+
+  .table {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .table * {
+    text-align: center;
+  }
+
+  .plan {
+    border-bottom: none;
+    border-right: 1px solid #e1f1ff;
+    flex-basis: 100%;
+    padding: 25px 50px;
+  }
+
+  .plan:last-child {
+    border-right: none;
+  }
+
+  .header {
+    color: #888;
+    font-weight: 600;
+    letter-spacing: 1px;
   }
 </style>
