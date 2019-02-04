a57c4d20b6c5e3a0c40a90ae4e38c09235b42d4a
Tyler Storm <stormtv@mail.uc.edu>
2019-02-04 18:45:32 -0500

    Fix vuex-electron vulnerability until PR is accepted

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -11,7 +11,7 @@ export default new Vuex.Store({
   modules,
   plugins: [
     createPersistedState({
-      blacklist: ['Onboarding/setWallet', 'Onboarding/setSeed']
+      ignoredPaths: ['Onboarding']
     }),
     createSharedMutations()
   ],
