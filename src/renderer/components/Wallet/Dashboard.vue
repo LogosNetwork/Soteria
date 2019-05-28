ce5189029c4beec7ca55fb17b8d04262d72c18be
Tyler Storm <stormtv@mail.uc.edu>
2019-05-28 19:49:40 -0400

    Accounts render

--- src/renderer/components/Wallet/Dashboard.vue
+++ src/renderer/components/Wallet/Dashboard.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="dashboard-container">
-    <b-container class="h-100" fluid>
-      <b-row class="h-100">
-        <b-col cols="3" class="pt-3">
+    <b-container class="h-100 p-0" fluid>
+      <b-row class="h-100" no-gutters>
+        <b-col cols="3" class="pt-4">
           <b-form-group v-if="wallet && wallet.accounts.length > 0">
             <div tabindex="-1" role="group">
-              <div id="accountSelectorRadios" role="radiogroup" tabindex="-1" class="btn-group-toggle btn-group-vertical">
-                <label class="btn btn-primary" v-bind:class="{ active: account.address === wallet.currentAccountAddress }" v-for="account in wallet.accounts" :key="account.address">
+              <div id="accountSelectorRadios" role="radiogroup" tabindex="-1" class="w-100 btn-group-toggle">
+                <label class="btn btn-transparent accountSelector" v-bind:class="{ active: account.address === wallet.currentAccountAddress }" v-for="account in wallet.accounts" :key="account.address">
                   <input @click="setCurrentAccount(account.address)" :id="`accountSelectorRadios_${account.address}`" type="radio" name="accountSelector" autocomplete="off" :value="account.address"/>
-                  <span>{{account.label}}</span>
+                  <div>{{account.label}}</div>
+                  <small>0 Logos</small>
                 </label>
               </div>
             </div>
@@ -50,8 +51,11 @@ export default {
     }
   },
   created () {
-    if (this.wallet.seed === null) this.$router.push({ path: '/' })
-    if (this.wallet && this.wallet.accounts.length === 0) this.addAccount()
+    if (this.wallet.seed === null) {
+      this.$router.push({ path: '/' })
+    } else if (this.wallet && this.wallet.accounts.length === 0) {
+      this.addAccount()
+    }
   }
 }
 </script>
@@ -61,6 +65,24 @@ export default {
     overflow:hidden;
     height: calc(100vh);
     background-color: var(--secondary);
+  }
+  .accountSelector {
+    text-align: left;
+    color: var(--gray-600);
+    margin-left: 1rem;
+    margin-right: 1rem;
+    width: calc(100% - 2rem);
+  }
+  .accountSelector.active,
+  .accountSelector:hover {
+    cursor: pointer;
+    color: var(--white);
+  }
+  .accountSelector.active {
+    background-color: var(--bg);
+  }
+  .accountSelector > small {
+    display: block;
   }
   .dashboard {
     background-color: var(--bg);
--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -11,7 +11,7 @@ export default new Vuex.Store({
   modules,
   plugins: [
     createPersistedState({
-      ignoredPaths: ['Onboarding', 'Wallet']
+      ignoredPaths: ['Onboarding']
     }),
     createSharedMutations()
   ],
