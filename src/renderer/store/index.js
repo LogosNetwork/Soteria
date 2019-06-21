0125ea96dbcca1fd2cdfdae77cd1cb27a7ad524b
Tyler Storm <stormtv@mail.uc.edu>
2019-06-21 11:35:15 -0400

    Add Web Application Persistence

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 import modules from './modules'
 
 Vue.use(Vuex)
 let plugins = []
+
 if (global && global.createPersistedState && global.createSharedMutations) {
+  // Electron Persistance
   plugins = [
     global.createPersistedState({
       ignoredPaths: ['Onboarding']
     })
   ]
+} else if (window.localStorage) {
+  // Web Application Persistance
+  plugins = [new VuexPersistence({
+    storage: window.localStorage,
+    supportCircular: true,
+    reducer: (state) => {
+      return {
+        EncryptedWallet: state.EncryptedWallet,
+        Language: state.Language,
+        Wallet: state.Wallet
+      }
+    }
+  }).plugin]
 }
 export default new Vuex.Store({
   modules,
