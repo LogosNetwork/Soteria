95793ed5d8c1322c1899c9e28b5f675662e4fc75
Tyler Storm <stormtv@mail.uc.edu>
2019-08-05 21:21:10 -0400

    Put create token back in

--- src/renderer/components/Wallet/Tokens/Nav.vue
+++ src/renderer/components/Wallet/Tokens/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tokenPanel scroller visible">
+  <div class="tokenPanel scroller">
     <b-form-group
       class="text-left searchWrapper pt-4 pb-3 mb-0"
     >
@@ -10,23 +10,48 @@
         trim
       />
     </b-form-group>
-    <div class="filterButtonWrapper">
+    <div class="filterButtonWrapper px-2">
+      <b-button
+        v-b-modal.createToken
+        variant="outline-primary"
+        class="align-top text-white w-100 mb-3"
+      >
+        <font-awesome-icon
+          :icon="['fal','plus']"
+          class="mr-2"
+        />
+        <span v-t="'createToken'" />
+      </b-button>
       <TokenSelectorButton
         v-for="account in $Wallet.tokenAccounts"
         :key="account.address"
         :token="account"
       />
     </div>
+    <b-modal
+      id="createToken"
+      body-class="pb-0"
+      modal-class="pl-0"
+      content-class="scroller"
+      size="full"
+      no-fade
+      hide-footer
+      hide-header
+    >
+      <CreateToken />
+    </b-modal>
   </div>
 </template>
 
 <script>
 import TokenSelectorButton from '@/components/Wallet/Tokens/Nav/TokenSelector.vue'
+import CreateToken from '@/components/Wallet/Tokens/CreateToken.vue'
 
 export default {
   name: 'TokenSelectorNav',
   components: {
-    TokenSelectorButton
+    TokenSelectorButton,
+    CreateToken
   },
   data () {
     return {
